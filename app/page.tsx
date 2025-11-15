edaccaae6b9d8b3f00a40efa3817b846200e7944
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2025-11-15 09:34:58 +0100

    feat: 10.1: add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { getWeekOverview } from "@/services/data/ServiceData";
-import { WeekCard } from "@/components/WeekCard";
+import { WeekCard } from "@/components/card/WeekCard";
+import { Header } from "@/components/header/Header";
 export default async function Home() {
   const weekOverview = await getWeekOverview();
   return (
     <div>
-      <main className="flex flex-wrap justify-center">
+      <Header />
+
+      <main className="flex flex-wrap justify-center pt-16">
         {weekOverview.overviews.map((w) => (
           <WeekCard key={w.week} {...w}></WeekCard>
         ))}
